9673286f4c6ec017d1bd583ba540c55c0b797a67
CannibalishToast <thesu912@gmail.com>
2025-10-18 03:27:00 -0500

    Initial Commit for Genomebank-API

---
+++ genomebank-api/src/test/java/com/genomebank/genomebank_api/GenomebankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genomebank.genomebank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomebankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
